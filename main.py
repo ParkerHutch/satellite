4afe1000ac5650cf1ef03252200bf9c46cef7b19
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-08 13:49:30 -0500

    use config file for server and port attributes

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ This message was sent from a Python script."""
 port = 465 # SSL
 context = ssl.create_default_context() # Create a secure SSL context
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+with smtplib.SMTP_SSL(
+    config['sender']['server'], config['sender']['port'], context=context
+) as server:
     server.login(config['sender']['username'], config['sender']['password'])
     server.sendmail(
         config['sender']['username'], config['recipient']['username'], message)
