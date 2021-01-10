874301a4084948ce2f3f60519133bea734d0d85e
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-09 22:31:44 -0500

    fixed double instantiation of camera

--- snapshot.py
+++ snapshot.py
@@ -1,11 +1,5 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
-
 with PiCamera() as camera:
     camera.capture('./images/image.jpg')
-#camera.start_preview()
-#sleep(5)
-
-#camera.stop_preview()
