a69b9ef332e9daad5eb3d478304dab19d9e0dd60
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-10 13:32:15 -0500

    detect cameras

--- snapshot.py
+++ snapshot.py
@@ -3,19 +3,35 @@ import subprocess
 from time import sleep
 
 try:
-    camera = PiCamera() # Make sure to close this
+    camera = PiCamera() # Make sure to close this on program end
 except:
     print('no official Raspberry Pi camera connected')
 
-def get_num_cameras():
-    cmd_output = subprocess.check_output(['script', '-c', '-q', '(fswebcam --list-inputs)']).decode('utf-8')
-    # TODO delete automatically created typescript file
-    start = 'Available inputs:'
-    end = 'No input was specified'
-    first_run = (cmd_output.split(start)[1]).split(end)[0]
-    last_semicolon_index = first_run.rfind(':')
-    largest_device_index = int(first_run[last_semicolon_index - 1: last_semicolon_index])
-    return largest_device_index + 1
+mounted_cameras = {}
+
+def find_cameras():
+    mounted_cameras = {}
+    for i in range(10):
+        num_cameras = get_num_cameras(i)
+        if num_cameras > 0:
+            mounted_cameras[i] = num_cameras
+
+
+# get the number of cameras installed for the given /dev/video mount.
+def get_num_cameras(mount_num):
+    cmd_output = subprocess.check_output(['script', '-q', '-c', f'(fswebcam --list-inputs -d /dev/video{mount_num})']).decode('utf-8')
+    error_messages = ['Unable to query input 0.', 'No such file or directory']
+    if any(message in cmd_output for message in error_messages):
+        print(f'Error for {mount_num}')
+        return 0
+    else:
+        # TODO delete automatically created typescript file
+        start = 'Available inputs:'
+        end = 'No input was specified'
+        first_run = (cmd_output.split(start)[1]).split(end)[0]
+        last_semicolon_index = first_run.rfind(':')
+        largest_device_index = int(first_run[last_semicolon_index - 1: last_semicolon_index])
+        return largest_device_index + 1
 
 
 def take_picture(device, output_file):
@@ -33,7 +49,7 @@ def stop():
     camera.close()
 
 if __name__ == 'main':
-    print('Taking a picture')
-    camera.capture('image.jpg')
-    print('Done')
+    print('finding cameras')
+    find_cameras()
+    print(mounted_cameras)
     stop()
