6bed61de8e57324121c3b6819538cb0e42f1ad4b
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-10 12:32:57 -0500

    print number of connected cameras

--- snapshot.py
+++ snapshot.py
@@ -7,14 +7,26 @@ try:
 except:
     print('no official Raspberry Pi camera connected')
 
+def get_num_cameras():
+    cmd_output = subprocess.check_output(['script', '-c', '(fswebcam --list-inputs)']).decode('utf-8')
+    # TODO delete automatically created typescript file
+    start = 'Available inputs:'
+    end = 'No input was specified'
+    first_run = (cmd_output.split(start)[1]).split(end)[0]
+    last_semicolon_index = first_run.rfind(':')
+    largest_device_index = int(first_run[last_semicolon_index - 1: last_semicolon_index])
+    return largest_device_index
+
+
 def take_picture(device, output_file):
     if device == 'picamera':
         camera.capture(output_file)
     elif device == 'webcam':
-        print('inputs found:')
-        inputs = subprocess.check_output(['fswebcam', '--list-inputs'])
-        print('inputs return: ')
-        print(inputs)
+        print('inputs found:', get_num_cameras())
+        
+        #inputs = subprocess.check_output(['fswebcam', '--list-inputs'], stderr=subprocess.STDOUT)
+        #print('inputs return: ')
+        #print(inputs)
         print('taking picture')
         subprocess.run(['fswebcam', '-r', '1280x720', '--no-banner', '-q', output_file])
     else:
