d3f918aef99297265ca94f612f53bd2dec009311
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-10 12:53:27 -0500

    quietly enter and exit script

--- snapshot.py
+++ snapshot.py
@@ -8,14 +8,14 @@ except:
     print('no official Raspberry Pi camera connected')
 
 def get_num_cameras():
-    cmd_output = subprocess.check_output(['script', '-c', '(fswebcam --list-inputs)']).decode('utf-8')
+    cmd_output = subprocess.check_output(['script', '-c', '-q', '(fswebcam --list-inputs)']).decode('utf-8')
     # TODO delete automatically created typescript file
     start = 'Available inputs:'
     end = 'No input was specified'
     first_run = (cmd_output.split(start)[1]).split(end)[0]
     last_semicolon_index = first_run.rfind(':')
     largest_device_index = int(first_run[last_semicolon_index - 1: last_semicolon_index])
-    return largest_device_index
+    return largest_device_index + 1
 
 
 def take_picture(device, output_file):
@@ -23,10 +23,6 @@ def take_picture(device, output_file):
         camera.capture(output_file)
     elif device == 'webcam':
         print('inputs found:', get_num_cameras())
-        
-        #inputs = subprocess.check_output(['fswebcam', '--list-inputs'], stderr=subprocess.STDOUT)
-        #print('inputs return: ')
-        #print(inputs)
         print('taking picture')
         subprocess.run(['fswebcam', '-r', '1280x720', '--no-banner', '-q', output_file])
     else:
