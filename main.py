8bc121c9ced57ad51875de09eb344f6d7fd2cb8b
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-11 11:10:25 -0500

    todo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import snapshot
 import email_handler
 
 #TODO make the images directory, if it doesn't exist errors occur
+#TODO time how long these functions take
 snapshot.take_picture()
 snapshot.stop()
 
