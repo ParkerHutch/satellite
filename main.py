3fed5fc5444eb930a8255d03227ad343a070122c
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-12 12:29:51 -0500

    quit after listing devices

--- main.py
+++ main.py
@@ -15,29 +15,29 @@ parser.add_argument('-d', '--device',
                     help='which device to use for capturing photos (specify all to use all devices)', 
                     type=str, default='all')
 parser.add_argument('-l', '--list-devices',
-                    help='list all detected devices', action='store_true')
+                    help='list all detected devices and quit', action='store_true')
 
 def main():
     args = parser.parse_args()
     
     if args.list_devices:
         print('Connected devices:', snapshot.find_devices().keys())
-    
-    capture_start = time.time()
-    snapshot.capture(camera_device=args.device, 
-                        add_processing=args.process_images, 
-                        verbose=args.verbose)
-    capture_end = time.time()
-    if args.verbose:
-        print(f'Taking pictures: \t{capture_end-capture_start} seconds')
-    snapshot.stop()
-
-    if not args.no_email:
-        email_start = time.time()
-        email_handler.send_email('images/')
-        email_end = time.time()
+    else:
+        capture_start = time.time()
+        snapshot.capture(camera_device=args.device, 
+                            add_processing=args.process_images, 
+                            verbose=args.verbose)
+        capture_end = time.time()
         if args.verbose:
-            print(f'Sending email: \t\t{email_end - email_start} seconds')
+            print(f'Taking pictures: \t{capture_end-capture_start} seconds')
+        snapshot.stop()
+
+        if not args.no_email:
+            email_start = time.time()
+            email_handler.send_email('images/')
+            email_end = time.time()
+            if args.verbose:
+                print(f'Sending email: \t\t{email_end - email_start} seconds')
 
 if __name__ == '__main__':
     main()
