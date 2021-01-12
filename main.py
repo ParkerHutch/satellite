06be286fc80767b51ad4eb88f549a2597ece953f
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-12 10:26:24 -0500

    measure time elapsed for sending email

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import time
 import snapshot
 import email_handler
 
-#TODO time how long these functions take
-import time
-start = time.time()
+capture_start = time.time()
 snapshot.take_picture()
-end = time.time()
-print(f'taking pictures took {end-start} seconds')
+capture_end = time.time()
+print(f'taking pictures took {capture_end-capture_start} seconds')
 snapshot.stop()
 
+email_start = time.time()
 email_handler.send_email('images/')
+email_end = time.time()
+print(f'Sending email took {email_end - email_start} seconds')
