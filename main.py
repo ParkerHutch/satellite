cb0aa01af56d66b5a28baaf1bf80413c27c750f7
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-12 10:18:05 -0500

    print time elapsed when taking pictures

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import snapshot
 import email_handler
 
 #TODO time how long these functions take
+import time
+start = time.time()
 snapshot.take_picture()
+end = time.time()
+print(f'timing took {start-end} seconds ')
 snapshot.stop()
 
 email_handler.send_email('images/')
