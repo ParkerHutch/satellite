ab1faccd4990211a16b8b3e089766bf2871e19af
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-14 15:19:57 -0500

    fixed memory info function to not include second line values

--- diagnostics.py
+++ diagnostics.py
@@ -4,9 +4,11 @@ from typing import Dict
 
 def get_memory_info(units_arg:str = '--mega') -> Dict[str, str]:
     output = subprocess.check_output(['free', '-w', '--mega'], text=True)
-    columns = output.split('Mem:')[0].split()
-    numbers = list(map(int, output.split('Mem:')[1].split()))
-    return dict(zip(columns, numbers))
+    
+    column_headers = output.split('Mem:')[0].split()[:7]
+    numbers = list(map(int, 
+                        output.split('Mem:')[1].split()[:len(column_headers)]))
+    return dict(zip(column_headers, numbers))
 
 
 if __name__ == '__main__':
