a9688cd4eda12fe6b83e555b70d4deb491c57380
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-13 23:12:13 -0500

    use != instead of 'is not'

--- snapshot.py
+++ snapshot.py
@@ -226,7 +226,8 @@ def capture(camera_device: str = 'all',
     if camera_device == 'all':
         # Take a picture on all connected cameras, excluding the PiCamera
         for device_name, cameras in find_devices().items():
-            if device_name is not 'RPi Camera Module':
+            print('device:', device_name)
+            if device_name != 'RPi Camera Module':
                 for _ in range(cameras):
                     take_fswebcam_picture(
                         device_name, 
