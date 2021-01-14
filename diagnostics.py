8554706099b6cc63d5ab753eb7f4aa67356aaf86
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-14 14:59:21 -0500

    made function for getting memory info

--- diagnostics.py
+++ diagnostics.py
@@ -1,8 +1,13 @@
 import subprocess
+from typing import Dict
 
-if __name__ == '__main__':
+
+def get_memory_info(units_arg:str = '--mega') -> Dict[str, str]:
     output = subprocess.check_output(['free', '-w', '--mega'], text=True)
     columns = output.split('Mem:')[0].split()
-    numbers = output.split('Mem:')[1].split()
-    memory_info = dict(zip(columns, numbers))
-    print(memory_info)
+    numbers = list(map(int, output.split('Mem:')[1].split()))
+    return dict(zip(columns, numbers))
+
+
+if __name__ == '__main__':
+    print(get_memory_info())
