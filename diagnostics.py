3a4f1ca904f0d5ffbe23622ed710f2db00e4e78f
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-14 14:31:41 -0500

    show used and available memory

--- diagnostics.py
+++ diagnostics.py
@@ -1,6 +1,7 @@
 import subprocess
 
 if __name__ == '__main__':
-    output = subprocess.check_output(
-        ['awk', '/^Mem/ {print $3}', '<(free -m)'])
-    print(output)
+    output = subprocess.check_output(['free', '-w'], text=True)
+    numbers = output.split('Mem:')[1].split()
+    print(f'Memory used: {numbers[0]} Available: {numbers[1]}')
+    #print(output)
