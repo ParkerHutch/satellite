cb79fef03f6b86fc5d8b360c0ef20ef2cbf4a693
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-14 14:51:42 -0500

    code for getting memory availability

--- diagnostics.py
+++ diagnostics.py
@@ -1,7 +1,8 @@
 import subprocess
 
 if __name__ == '__main__':
-    output = subprocess.check_output(['free', '-w'], text=True)
+    output = subprocess.check_output(['free', '-w', '--mega'], text=True)
+    columns = output.split('Mem:')[0].split()
     numbers = output.split('Mem:')[1].split()
-    print(f'Memory used: {numbers[0]} Available: {numbers[1]}')
-    #print(output)
+    memory_info = dict(zip(columns, numbers))
+    print(memory_info)
