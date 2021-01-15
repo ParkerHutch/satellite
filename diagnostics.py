05c8e1f9958586d7823ff1f99ba17785e6f5f9cb
Parker Hutchinson <parker.hutch@outlook.com>
2021-01-15 14:27:46 -0500

    documentation and refactoring

--- diagnostics.py
+++ diagnostics.py
@@ -5,11 +5,18 @@ import psutil
 import humanize
 import platform
 
-def get_cpu_usage_percent():
+def get_cpu_usage_percent() -> float:
+    """Get the system's CPU utilization percentage."""
     return psutil.cpu_percent()
 
 
-def get_wifi_signal_strength():
+def get_wifi_signal_strength() -> float:
+    """Get the wifi signal strength as a percentage. This function extracts the
+    output from the 'iwconfig' command to get the strength value.
+
+    Returns:
+        float: the wifi signal strength percentage
+    """
     output = subprocess.check_output(['iwconfig'], text=True, 
                                         stderr=subprocess.DEVNULL)
     strength_fraction = output.split('Link Quality=')[1].split()[0]
@@ -17,35 +24,71 @@ def get_wifi_signal_strength():
     return (numerator / denominator) * 100
 
 
-def get_memory_info():
+def get_memory_used_percent() -> float:
+    """Get the percentage of memory on the system that has been used out of the
+    total available memory.
+
+    Returns:
+        float: the system's memory used percentage
+    """
     mem = psutil.virtual_memory()
-
     used_percent = (float(mem.used) / mem.available) * 100
-    return {
-        'Used Percentage': used_percent, 
-        'Memory Available': humanize.naturalsize(mem.available)
-    }
+    return used_percent
 
+def get_memory_available() -> int:
+    """Return the system's available memory in bytes."""
+    return psutil.virtual_memory().available
 
-def get_system():
+def get_memory_available_human() -> str:
+    """Return the system's available memory in a human readable form."""
+    return humanize.naturalsize(get_memory_available())
+
+def get_system() -> str:
+    """Return the platform's system (Linux, Windows, etc.) if it can be found, 
+    otherwise 'Unknown'
+
+    Returns:
+        str: the platform's system or 'Unknown'
+    """
     return platform.system() if platform.system() else 'Unknown'
 
 
-def get_processor():
+def get_processor() -> str:
+    """Return the platform's processor if it can be found, otherwise 'Unknown'
+
+    Returns:
+        str: the platform's processor or 'Unknown'
+    """
     return platform.processor() if platform.processor() else 'Unknown'
 
-def get_temperature():
+def get_temperature() -> float:
+    """Get the computer hardware's temperature in degrees Farenheit. If multiple
+    temperature sensors are present, this returns the first sensor's 
+    temperature.
+
+    Returns:
+        float: the computer hardware's temperature in degrees Farenheit
+    """
+
     temps = psutil.sensors_temperatures(fahrenheit=True)
     first_temp = next(iter(temps.values()))[0].current
     return first_temp
 
-def get_diagnostics(): # TODO rename formatted diagnostics
+def get_formatted_diagnostics() -> Dict[str, any]:
+    """Get computer diagnostics as a dictionary with attributes as keys and 
+    their formatted values as values.
+
+    Returns:
+        Dict[str, any]: a dictionary of diagnostic attributes, where values have
+        been formatted so as to be displayable to a user
+    """
+
     return {
         'CPU Usage': f'{get_cpu_usage_percent():.1f}%',
         'Wifi Strength': f'{get_wifi_signal_strength():.1f}%',
         'Temperature:': f'{get_temperature():.1f}\u00b0F',
-        'Memory Used': f'{get_memory_info()["Used Percentage"]:.1f}%',
-        'Memory Available': f'{get_memory_info()["Memory Available"]}',
+        'Memory Used': f'{get_memory_used_percent():.1f}%',
+        'Memory Available': f'{get_memory_available()}',
         'System': get_system(),
         'Processor': get_processor()
     }
